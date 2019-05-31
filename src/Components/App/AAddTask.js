3e671cc822868f540693d124bdeda93d794cc074
belkaz <belkaz@gmail.com>
2019-05-31 16:16:45 +0300

    zero stage

--- src/Components/App/AAddTask.js
+++
@@ -1,8 +0,0 @@
-let AAddTask = ( taskObj ) => {
-    return {
-        type : 'ADD_TASK',
-        data : taskObj        
-    }
-}
-
-export default AAddTask;
--- src/Components/Task/AcloseTask.js
+++
@@ -1,8 +0,0 @@
-let ACloseTask = ( taskId) => {
-    return {
-        type : 'CLOSE_TASK',
-        taskId : taskId        
-    }
-}
-
-export default ACloseTask;
