1af193f62827e340cb3474bc33b1a2998a73dd1b
belkaz <belkaz@gmail.com>
2019-06-04 17:32:19 +0300

    йцу

--- src/Components/AddTask/AddTask.js
+++ src/Components/AddTask/AddTask.js
@@ -5,14 +5,48 @@ import AAddTask from './AAddTask';
 import './AddTask.sass';
 
 class AddTask extends Component {
-    clickHandler = ( ) => {    
+    constructor ( props ) {
+        super ();
+        this.state = {
+            selectedValue : 0
+        }
+    }
+    clickHandler ( ) {    
         this.props.tryToAddTask ( )              
       }; 
+    genAddTask () {
+        switch ( this.state.selectedValue ) {
+            case 'Transfer' : {
+                
+            };
+            case 'TempAddRights' : {
+
+            };
+            case 'CloseUser' :{
+
+            };
+            case 'LockUser' : {
+
+            };
+            default : {}
+        }
+    }
     render() {
         return (
             <div 
                 className = 'AddTask'
                 style = { { 'opacity' : this.props.v/1 }}>
+                <select 
+                    className = 'AddTaskType'
+                    onChange = { ( e )=> { 
+                        var tt = e.target;
+                        this.setState ({ selectedValue : tt.options[ tt.selectedIndex].value });                       
+                    } } >
+                    <option>Transfer</option>
+                    <option>TempAddRights</option>
+                    <option>CloseUser</option>
+                    <option>LockUser</option>
+                </select>
 
             </div>
         )
