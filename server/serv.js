92dfda3e196f198eed052aabf6a24c7484ad3243
belkaz <targetnlacc@gmail.com>
2019-06-06 15:09:48 +0300

    simple express serv

---
+++ server/serv.js
@@ -0,0 +1,52 @@
+const express = require ('express');
+const app = express();
+const fs = require ('fs');
+
+let files = [];
+
+fs.readdirSync('./users/').forEach( el => {
+    files.push ( el )
+});
+
+let allUsersData = [];
+
+files.forEach( el => {
+    try {
+        let ob = JSON.parse( fs.readFileSync('./users/' +el, 'utf8'));
+        allUsersData.push ( ob ) 
+    }
+    catch ( err ) {
+        console.log ( el )
+    }
+});
+
+function FindUnclosedTasts () {
+    let rez = [];
+    allUsersData.forEach ( el => {
+        let logsArr = el.LOGS;
+        logsArr.forEach ( al => {
+            if ( al.CLOSED === "-") {
+                rez.push({
+                    TYPE : al.TYPE,
+                    WHO : el.FIO,
+                    FROM : al.FROM,
+                    TO : al.TO,
+                    START : al.START,
+                    END: al.END,
+                    REASON : al.REASON
+                })
+            }
+        })
+    })
+    return rez;
+}
+
+app.get('/unclosed', function ( req, res, next ) {
+    res.send( FindUnclosedTasts() )
+    next();
+});
+
+
+app.listen( 9999, function() {
+    console.log ( 'SERVER ON' )
+});
